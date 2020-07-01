74f603194ffd9be2c900770c7a5086fa662ef666
Eugenia Petrunia <avepetrunia@gmail.com>
2020-07-01 12:37:41 +0300

    Add solution for second task

--- index.js
+++ index.js
@@ -17,3 +17,21 @@
 // } else {
 //     alert('нет');
 // }
+
+// 2. необходимо создать функцию hasElem, которая параметром будет принимать массив и возвращать true, если 'c' есть в массиве, и false - если нет
+// /**
+//  *
+//  * @param arr
+//  * @returns {boolean}
+//  */
+// function hasElem(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === 'c') {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+//
+// const array = ['a', 2, 'c', true];
+// alert(hasElem(array));
