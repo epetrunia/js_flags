ba4ee55ecc1ba3223ff0a144efbbd894cdfe6663
Eugenia Petrunia <avepetrunia@gmail.com>
2020-07-01 12:25:31 +0300

    Add solution for first task

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1. дан массив, нужно проверить, есть ли в нем элемент со значением 'c' или нет. Если есть - вывести 'есть', если нет - вывести 'нет'.
+// /**
+//  *
+//  * @type {(string|number|boolean)[]}
+//  */
+// const array = ['a', 2, 'c', true];
+// let isElement = false;
+//
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'c') {
+//         isElement = true;
+//     }
+// }
+//
+// if (isElement) {
+//     alert('есть');
+// } else {
+//     alert('нет');
+// }
